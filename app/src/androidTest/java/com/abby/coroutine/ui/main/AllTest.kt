9cd8d6f12aec2a1e9ac5eed4164aa25aa8cb2ec4
wxmylife <wxmylife@gmail.com>
2021-03-06 16:54:29 +0800

    添加 Suite ，执行全部测试

---
+++ app/src/androidTest/java/com/abby/coroutine/ui/main/AllTest.kt
@@ -0,0 +1,12 @@
+package com.abby.coroutine.ui.main
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+  MainActivityTest::class,
+  SecondaryActivityTest::class
+)
+class AllTest {
+}
